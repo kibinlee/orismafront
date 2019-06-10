0763d8b040803ab640506eaf01691da527bab4e5
John Lee <faker@Johns-MacBook-Pro.local>
2019-06-10 23:42:52 +0900

    Fotter and Auth UI part Three

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { gql } from "apollo-boost";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
 import { useQuery } from "react-apollo-hooks";
+import Footer from "./Footer";
 
 const QUERY = gql`
   {
@@ -12,19 +13,26 @@ const QUERY = gql`
   }
 `;
 
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%;
+`;
+
 export default () => {
   // #4.4 3:00
   const {
     data: { isLoggedIn }
   } = useQuery(QUERY);
-  console.log(isLoggedIn);
+  // console.log(isLoggedIn);
 
   return (
     <ThemeProvider theme={Theme}>
-      <>
+      <Wrapper>
         <GlobalStyles />
         <Router isLoggedIn={isLoggedIn} />
-      </>
+        <Footer />
+      </Wrapper>
     </ThemeProvider>
   );
 };
